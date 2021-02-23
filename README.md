b488797934bf0dceb3c3ac457580425783300d53
daniel-kanchev <daniel.kanchev@adata.pro>
2021-02-23 11:44:32 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,10 @@
+URL: https://www.bpdg.ch/fr/news
+
+    Spider name: bpdg
+
+DB Schema:
+- title
+- date
+- link
+- content
+
---
+++ bpdg/settings.py
@@ -0,0 +1,11 @@
+BOT_NAME = 'bpdg'
+SPIDER_MODULES = ['bpdg.spiders']
+NEWSPIDER_MODULE = 'bpdg.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'bpdg.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+LOG_LEVEL = 'WARNING'
+# LOG_LEVEL = 'DEBUG'
